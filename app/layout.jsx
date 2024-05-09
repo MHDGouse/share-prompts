62c14fa289a478a59aed957fe4b7702c9daefe8d
Mohammed Gouse <gpasha798@gmail.com>
2024-05-09 22:20:41 +0530

    adding files to the repo

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+import '@styles/globals.css'
+
+export const metadata={
+    "title":"promtopia",
+    "description": "Discover & share AI prompts",
+}
+const RootLayout = () => {
+  return (
+   <html>
+    <body>
+        <div className='main'>
+            <div className='gradi'></div>
+        </div>
+    </body>
+   </html>
+  )
+L}
+
+export default Rootlayout;
